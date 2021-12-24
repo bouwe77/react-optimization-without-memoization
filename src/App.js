524f1b9f2e77799b67c806e537a5fc53992d5d88
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-12-24 22:08:13 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import { useState, useEffect } from "react";
+import stuff from "./stuff";
 
-// Pass [component + props] as a prop
-// Render children coming from parent
-// Create const that has [component + props] outside of the component
+// Three ways of optimizing without memoization:
+// [1] Render children coming from parent
+// [2] Pass [component + props] as a prop
+// [3] Create const that has [component + props] outside of the component
 
 // Use this hook to be able to rerender a component when the component has no state.
+// Usage: Call the hook, and then call the function returned by the hook when you want to rerender.
+// const rerender = useForceRerender();
 function useForceRerender() {
   const [, setState] = useState();
+  useEffect(() => console.log("render..."));
   return () => setState((prevState) => !prevState);
+}
+
+function getStuff() {
+  return new Promise((resolve, _) => {
+    setTimeout(() => {
+      resolve(stuff);
+    }, 500);
+  });
+}
+
+function useStuff() {
+  const [stuff, setStuff] = useState([]);
+  const [status, setStatus] = useState("idle");
+
+  useEffect(() => {
+    (async () => {
+      setStatus("loading");
+      try {
+        const stuff = await getStuff();
+        setStatus("success");
+        setStuff(stuff);
+      } catch (error) {
+        setStatus("error");
+      }
+    })();
+  }, []);
+
+  return { stuff, status };
 }
 
 export default function App() {
   const rerender = useForceRerender();
-
-  useEffect(() => console.log("rendered"));
+  const { stuff, status } = useStuff();
 
   return (
     <div>
       <button onClick={rerender}>Click to rerender</button>
+
+      {status === "loading" ? (
+        <div>loading...</div>
+      ) : status === "error" ? (
+        <div>An error occurred...</div>
+      ) : (
+        <div>
+          {stuff.map((s) => (
+            <div key={s.id}>{s.name}</div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/stuff.js
@@ -0,0 +1,5 @@
+export default [
+  { id: 1, name: "Miep" },
+  { id: 2, name: "Kees" },
+  { id: 3, name: "Piet" }
+];
